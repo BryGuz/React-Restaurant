f7f30f81f299d06567063dd8b232b5ef27653b05
Bryan Guzmán <bmguzmang@unal.edu.co>
2020-10-16 17:33:23 -0500

    react-redux-form added

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -2,18 +2,24 @@ import React from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
 
 /*import { useState } from 'react';*/
 
+const store = ConfigureStore();
 
 function App() {
-
+  
   return (
-    <BrowserRouter>
-      <div>
-        <Main/>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <Main/>
+        </div>
+      </BrowserRouter>
+    </Provider>
+    
   );
 }
 
